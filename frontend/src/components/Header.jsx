c3f689c6b5584ed9e14802f2817cca1c9a07913a
HoreaMatei <mateihorea17@yahoo.com>
2025-04-07 11:04:29 +0300

    header change

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="relative text-center text-[#7c3331]  mt-24 sm:mt-20 lg:mt-56 ">
+    <header className="relative text-center text-[#7c3331]  mt-24 sm:mt-20 lg:mt-[18vh] ">
       <h1 className="text-2xl  sm:text-3xl lg:text-6xl  font-bold font-mono z-10">
         AI Background Remover
       </h1>
